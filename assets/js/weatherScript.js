c14a6841778c4dcf0b5efa5007f0516f57209ed8
Shawn Lapuz <lapuzshawn@gmail.com>
2023-05-16 11:53:26 -0700

    added boiler plate bootstrap HTML with hardcoded placehoders

---
+++ assets/js/weatherScript.js
@@ -0,0 +1,6 @@
+// Global variables
+var searchHistory = [];
+
+var weatherApiKey = 'b38319b9575fb449167d2125fd88dd05';
+
+var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${weatherApiKey}`;
